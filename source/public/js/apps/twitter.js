52bfa2ae0251e1e9979f68a7243c24c35a96c5c6
rhiokim <rhio.kim@gmail.com>
2012-04-17 22:45:10 +0900

    issue drop! gone to backlog

--- source/public/js/apps/twitter.js
+++ source/public/js/apps/twitter.js
@@ -61,18 +61,27 @@ function showTwitterFeed(tweets, twitter_user) {
   var content = '';
 
   for (var t in tweets) {
-    content += '<p>'+'<a href="http://twitter.com/'+twitter_user+'/status/'+tweets[t].id_str+'" class="pull-right">'+prettyDate(tweets[t].created_at)+'</a>'+linkifyTweet(tweets[t].text.replace(/\n/g, '<br>'), tweets[t].entities.urls)+'</p>';
+    content += '<p><a href="http://twitter.com/'+ twitter_user +'/status/'+ tweets[t].id_str +'" class="pull-right label">'+ prettyDate(tweets[t].created_at) +'</a>'+ linkifyTweet(tweets[t].text.replace(/\n/g, '<br>'), tweets[t].entities.urls) +'</p>';
     content += '<hr/>'
   }
     $(content).appendTo('._tweets');
 }
 
+
 function getTwitterFeed(user, count, replies) {
-  count = parseInt(count, 10);
+    $('._tweets .bar').animate({width: '100%'});
+
+    count = parseInt(count, 10);
+
     $.jsonp({
-        url: "http://api.twitter.com/1/statuses/user_timeline/" + user + ".json?trim_user=true&count=" + (count + 20) + "&include_entities=1&exclude_replies=" + (replies ? "0" : "1") + "&callback=?"
-        , type: 'jsonp'
-        , error: function (err) { $('#_tweets').text("Twitter's busted"); }
-        , success: function(data) { showTwitterFeed(data.slice(0, count), user); }
+        url: "http://api.twitter.com/1/statuses/user_timeline/" + user + ".json?trim_user=true&count=" + (count + 20) + "&include_entities=1&exclude_replies=" + (replies ? "0" : "1") + "&callback=?",
+        type: 'jsonp',
+        error: function (err) { $('#_tweets').text("Twitter's busted"); },
+        success: function(data) {
+            showTwitterFeed(data.slice(0, count), user);
+
+            $('._tweets .bar').width('100%');
+            $('._tweets  .progress').hide();
+        }
     });
 }
