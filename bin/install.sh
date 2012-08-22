93a2da89a6f189dabc245bbb514076a4c4454268
rhiokim <rhio.kim@gmail.com>
2012-08-22 20:29:22 +0900

    bump

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-SITE_NAME=$1
-OUTPUT_DIR=$2
+OUTPUT_DIR=$1
 TEMPRORY_FILE=/tmp/haroopress.tar.gz
 
 # node install nvm
@@ -16,12 +15,8 @@ if [ ! -d "$OUTPUT_DIR" ]; then
     export OUTPUT_DIR=$(pwd)
 fi
 
-if [ ! -d "$SITE_NAME" ]; then
-    export SITE_NAME="haroopress"
-fi
-
 curl -Lk https://github.com/rhiokim/haroopress/zipball/master -o $TEMPRORY_FILE
 
-mkdir "$OUTPUT_DIR/$SITE_NAME"
+mkdir -p $OUTPUT_DIR
 
-unzip /tmp/haroopress.tar.gz -d "$OUTPUT_DIR/$SITE_NAME"
+unzip /tmp/haroopress.tar.gz -d "$OUTPUT_DIR"
