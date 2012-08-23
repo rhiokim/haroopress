c2703d980d77d9431f8c70fdda5d80f0f4bd941e
rhiokim <rhio.kim@gmail.com>
2012-08-24 01:26:47 +0900

    #218, init fancy install

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-OUTPUT_DIR=$1
+OUTPUT_DIR=$(pwd)
+SITE_DIR=$1
 TEMPRORY_FILE=/tmp/haroopress.tar.gz
 
 # node install nvm
@@ -11,12 +12,11 @@ echo "========================="
 echo "= haroopress installing =\n"
 echo "========================="
 
-if [ ! -d "$OUTPUT_DIR" ]; then
-    export OUTPUT_DIR=$(pwd)
+if [ ! -d "$SITE_DIR" ]; then
+    export SITE_DIR='haroopress'
 fi
 
 curl -Lk https://github.com/rhiokim/haroopress/zipball/master -o $TEMPRORY_FILE
 
-mkdir -p $OUTPUT_DIR
-
 unzip /tmp/haroopress.tar.gz -d "$OUTPUT_DIR"
+mv ./rhiokim-haroopress* $SITE_DIR
